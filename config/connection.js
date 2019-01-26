0e0a8ddbc659f4b26786440a76cbd590188593e4
Jared Wheet <jaredwheet@gmail.com>
2019-01-26 09:57:03 -0700

    fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,20 @@
 var mysql = require('mysql')
 var env = require("dotenv").config()
 var sqlPort = process.env.PORT || 3306
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
   host: 'localhost',
   port: sqlPort,
   user: 'root',
   password: process.env.db_password,
   database: 'burgers_db'
 })
-
+}
 // Make connection.
 connection.connect(function (err) {
   if (err) {
