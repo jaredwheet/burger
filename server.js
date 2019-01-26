7639776d6ac09b696b7b870bc67b21bc182856db
Jared Wheet <jaredwheet@gmail.com>
2019-01-25 21:13:27 -0700

    updates to server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'))
 
 var express = require('express')
 
-var PORT = process.env.PORT || 8080
 
 var app = express()
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'))
 
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+var PORT = process.env.PORT || 8080
 // Set Handlebars.
 var exphbs = require('express-handlebars')
 
